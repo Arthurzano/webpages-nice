4ed6ef2bcf38de7ac3445a44b74fedb840d70991
Arthurzano <159539643+Arthurzano@users.noreply.github.com>
2024-10-21 08:00:31 -0300

    Add files via upload

---
+++ KENDRICK LAMAR/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'BASKAS',
+    'WHAT IF?',
+    'DERRICK ROSE'
+)
+
+criaCartao(
+    'GANG',
+    'MAIOR DISS DA HISTÓRIA?',
+    'NOT LIKE US- KENDRICK LAMAR'
+)
+
+criaCartao(
+    'MICHAEL JORDAN',
+    'QUEM É O GOAT?',
+    'THE BLACK JESUS'
+)
+
+criaCartao(
+    'KENDRICK LAMAR MUSIC',
+    'Alright?',
+    'Alls my life I has to fight, nigga'
+)
---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Armas',
+    'Qual arma mata com 1 tiro no peito?',
+    'Operator'
+)
+
+criaCartao(
+    'Agentes',
+    'Chefe do protocolo valorant?',
+    'Brimstone'
+)
+
+criaCartao(
+    'Mapas',
+    'Mapas com 3 bombs?',
+    'Haven e Lotus'
+)
+
+criaCartao(
+    'Mapas',
+    'Ultimo mapa adicionado?',
+    'Abism'
+)
